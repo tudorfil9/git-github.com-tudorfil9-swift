7b1a9d720b6e0735b51dfa26d838042a45459090
Tudor <tudorfil@tudors-macbook-pro.local>
2015-12-13 12:46:14 +0200

    first ver

---
+++ swiftApp0/swiftApp0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftApp0
+//
+//  Created by Tudor Fil on 13/12/15.
+//  Copyright © 2015 twodoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
